68595dec4a7f7be3be79428194c34f3ce11bae7f
R.oldmee <280468014@qq.com>
2018-12-03 23:00:55 +0800

    Initial commit

---
+++ src/com/oldmee/controller/HelloMvcController.java
@@ -0,0 +1,18 @@
+package com.oldmee.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloMvcController {
+
+    @RequestMapping("hello/mvc")
+    @ResponseBody
+    public String helloMvc() {
+        System.out.println("hello");
+        return "hello";
+    }
+}
